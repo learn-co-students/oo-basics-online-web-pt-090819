baa5cf4fdfb1455e23bfe0161ced4606a6dd5057
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-25 00:03:06 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,31 @@
-
-
+class Book 
+  attr_accessor :author, :genre, :page_count
+  attr_reader :title
+  def initialize(title)
+    @title = title 
+  end 
+  
+  def title 
+    @title
+  end 
+  
+  def author
+    @author
+  end 
+  
+  def page_count
+    @page_count
+  end 
+  
+  def page_count
+    @page_count 
+  end 
+  
+  def genre 
+    @genre 
+  end
+  
+  def turn_page 
+    puts "Flipping the page...wow, you read fast!"
+  end
+end 

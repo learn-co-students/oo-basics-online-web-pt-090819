f065ddcbbc23a27604887565c7414b2a7c5d1e00
Charity Parks <tuppercharity@gmail.com>
2019-09-25 21:38:04 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+class Shoe         # Make your shoe class here!
+attr_accessor :color, :size, :material, :condition
+
+attr_accessor :brand
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def cobble
+    puts "Your shoe is as good as new!"
+      self.condition = "new"
+    end
+end
